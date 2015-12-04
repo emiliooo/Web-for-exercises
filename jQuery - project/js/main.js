0dd356edbdac01fcfcd7493c15302ac73916f520
emiliooo <emilburaczynski@gmail.com>
2015-12-04 12:06:24 +0100

    Initial commit

---
+++ jQuery - project/js/main.js
@@ -0,0 +1,90 @@
+
+//$(document).ready(function() {
+//
+//        $('.meu').on('click',function() {
+//            $('.meu li').fadeToggle(500);
+//            if ( $('ul').hasClass('notVis')) {
+//
+//
+//            } else {
+//                $('.meu li').fadeToggle(500);
+//                $('ul').removeClass('no').addClass('notVis')
+//
+//            }
+//        })
+//
+//
+//});
+//
+
+
+
+
+
+//$('.box1').on('click',function() {
+//    alert('asdf')
+//})
+//
+//
+//
+//$("#show").click(function(){
+//
+//    $("p").show(600, function(){
+//
+//        console.log("Funkcja ukonczona");
+//
+//
+//
+//
+
+//$(document).ready(function() {
+//$( "li" ).eq( 3 ).css( "background-color", "red" );
+//
+//});
+//
+//    //
+//    $(document).ready(function() {
+//    $('#menu').on('click',function() {
+//        alert('hurra ')
+//    })
+//
+//});
+
+
+
+
+    //$('p').on('click',function() {
+    //
+    //    alert('kliknales mnie ')
+    //
+    //})
+
+
+
+
+
+    //$('#but1').on('click',function() {
+    //
+    //    alert('kliknales mnie ')
+    //
+    //})
+    //
+    //$('#but2').one('click',function() {
+    //
+    //    alert('kliknales mnie ')
+    //
+    //})
+    //$('#but3').off('click')
+    //
+
+
+
+        //$('.box1').on('click',function() {
+        //    $('div').hide(2000);
+        //})
+        //
+        //$('.box3').on('click',function() {
+        //    $('div').toggle(200);
+        //})
+        //
+
